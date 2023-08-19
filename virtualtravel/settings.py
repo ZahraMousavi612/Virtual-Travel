f05375a6a36a47b914178e46915a61cfaa38e443
ZahraMousavi612 <>
2023-08-19 09:52:32 +0000

    deployment section

--- virtualtravel/settings.py
+++ virtualtravel/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'virtual-travel-078c66fa8b87.herokuapp.com'
+    'virtual-travel-078c66fa8b87.herokuapp.com',
     'virtual-travel.herokuapp.com',
     '8000-zahramousavi612-virtual-68c3nz07cm.us2.codeanyapp.com'
 ]
